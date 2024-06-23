3196990e3a876c0a81491a9fc7ae83cbd21ce83d
Jittipat-ph <Jittipat.ph@gmail.com>
2024-06-23 13:50:39 +0700

    Change skills

--- src/app/_components/Skill.tsx
+++ src/app/_components/Skill.tsx
@@ -10,6 +10,7 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Card from "./common/Card";
+import { faNodeJs } from "@fortawesome/free-brands-svg-icons/faNodeJs";
 
 type Props = {};
 
@@ -18,36 +19,20 @@ const Skill = (props: Props) => {
     <Card title="Skill">
       <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
         <div className="text-center">
-          <FontAwesomeIcon icon={faPhp} className="h-10" />
-          <p>PHP</p>
-        </div>
-        <div className="text-center">
           <FontAwesomeIcon icon={faPython} className="h-10" />
           <p>Python</p>
-        </div>
-        <div className="text-center">
-          <FontAwesomeIcon icon={faRust} className="h-10" />
-          <p>Rust</p>
-        </div>
-        <div className="text-center">
-          <FontAwesomeIcon icon={faReact} className="h-10" />
-          <p>React</p>
-        </div>
-        <div className="text-center">
-          <FontAwesomeIcon icon={faJava} className="h-10" />
-          <p>Java</p>
         </div>
         <div className="text-center">
           <FontAwesomeIcon icon={faJs} className="h-10" />
           <p>JS</p>
         </div>
         <div className="text-center">
-          <FontAwesomeIcon icon={faDocker} className="h-10" />
-          <p>Docker</p>
+          <FontAwesomeIcon icon={faNodeJs} className="h-10" />
+          <p>NodeJS</p>
         </div>
         <div className="text-center">
-          <FontAwesomeIcon icon={faVuejs} className="h-10" />
-          <p>Vue.js</p>
+          <FontAwesomeIcon icon={faDocker} className="h-10" />
+          <p>Docker</p>
         </div>
       </div>
     </Card>
