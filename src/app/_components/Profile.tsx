27f945dad61ec987f3c0e1a4028069640d18d069
Jittipat-ph <Jittipat.ph@gmail.com>
2024-06-23 10:45:45 +0700

    Change profile pic and details

--- src/app/_components/Profile.tsx
+++ src/app/_components/Profile.tsx
@@ -7,29 +7,29 @@ const Profile = (props: Props) => {
   return (
     <div className="bg-blue-100 h-full p-4 rounded-xl px-4">
       <div className="flex justify-center items-center pt-4 pb-2">
-        <Image
-          src="/profile.webp"
-          width={500}
-          height={500}
+        <img
+          src="https://media.tenor.com/1EhdUZSVCo8AAAAM/hakos_baelz-holo_council.gif"
+          // width={500}
+          // height={500}
           alt="profile picture"
           className="w-3/4 aspect-square rounded-full border-4 border-white"
         />
       </div>
       <div className="text-center">
-        <h1 className="text-2xl font-bold">John Farmer</h1>
-        <p className="text-base">Software Engineer</p>
+        <h1 className="text-2xl font-bold">Hako Baelz</h1>
+        <p className="text-base">RRat</p>
       </div>
 
-      <div className="py-10 text-center ">" May the force be with you "</div>
+      <div className="py-10 text-center ">"JDON My Soul"</div>
       <div className="p-6 mx-1 xl:mx-6 bg-blue-300 rounded-md">
         <p>
-          <b>Age:</b> 26
+          <b>Age:</b> 14 (not really)
         </p>
         <p>
           <b>Status:</b> Single
         </p>
         <p>
-          <b>Location:</b> Bangkok
+          <b>Location:</b> Japan
         </p>
       </div>
     </div>
