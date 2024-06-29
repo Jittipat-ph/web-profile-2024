02b8fc4008d82f2b09c0c22c0e83cbaf3be85191
Jittipat-ph <Jittipat.ph@gmail.com>
2024-06-29 22:11:01 +0700

    Change web title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ config.autoAddCss = false;
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "John Farmer",
+  title: "Jittipat Phedseng",
   description: "Example personal website",
 };
 
