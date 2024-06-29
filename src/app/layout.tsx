1553d9e290db4584cf13d91eb96a0b2237785b8f
Jittipat-ph <Jittipat.ph@gmail.com>
2024-06-29 22:39:52 +0700

    Undo the appName Changing.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,6 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Jittipat Phedseng",
-  applicationName: "Jittipat Phedseng",
   description: "Example personal website",
 };
 
